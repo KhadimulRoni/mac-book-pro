e3418b1ca6f142451681f9ff78d208a44aaa6da5
KhadimulRoni <mdkhadimul7066@gmail.com>
2021-08-21 13:44:28 +0600

    costs added

--- mac.js
+++ mac.js
@@ -1,12 +1,27 @@
 // -----Extra memory cost------
 const memoryBtn = document.getElementById('memory-btn').addEventListener('click',function(){
     const memoryCost = document.getElementById('memory-cost');
-    memoryCost.innerText =180; 
-    const total = document.getElementById('total');
-    total.innerText = 1299 + parseInt(memoryCost.innerText) ;
+    memoryCost.innerText = 180;
 });
 
 // ------Extra storage cost-------
-const ssd512gb = document.getElementById('ssd-512gb').addEventListener('click',function(){
-    const storageCost = 
-})
+const ssd512Gb = document.getElementById('ssd-512gb').addEventListener('click',function(){
+    const storageCost = document.getElementById('storage-cost');
+    storageCost.innerText = 100;
+});
+
+const ssd1Tb = document.getElementById('ssd-1tb').addEventListener('click', function(){
+    const storageCost = document.getElementById('storage-cost');
+    storageCost.innerText = 180;
+});
+
+// ------Delivery cost -----
+const regularDelivery = document.getElementById('regular-delivery').addEventListener('click', function(){
+    const deliveryCharge = document.getElementById('delivery-charge');
+    deliveryCharge.innerText = 20;
+});
+
+// ---- Total calculation -----
+function calculateTotal(){
+
+}
